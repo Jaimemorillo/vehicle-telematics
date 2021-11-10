c02d79e63f97263771cbff03c2720694ad98efa1
Jaime <jaimorile@hotmail.es>
2021-11-10 11:27:08 +0100

    Update main

---
+++ src/main/java/org/cloud/vehicletelematics/Main.java
@@ -0,0 +1,14 @@
+package org.cloud.vehicletelematics;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.aggregation.Aggregations;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        // set up the execution environment
+
+    }
+}
