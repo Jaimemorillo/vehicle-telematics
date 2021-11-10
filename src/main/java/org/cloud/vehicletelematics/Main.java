b57fbc8a4b8c416efe8db75ed2883dad39948d16
Jaime <jaimorile@hotmail.es>
2021-11-10 12:07:21 +0100

    Update Main

--- src/main/java/org/cloud/vehicletelematics/Main.java
+++ src/main/java/org/cloud/vehicletelematics/Main.java
@@ -1,12 +1,27 @@
 package org.cloud.vehicletelematics;
 
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.io.CsvReader;
+import org.apache.flink.api.java.operators.DataSource;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         // set up the execution environment
         //data/sample-traffic-3xways.csv
+
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+        String inFilePath = "./data/sample-traffic-3xways.csv";
+        String outFilePath = "./data/sample-traffic-out.csv";
+        CsvReader csvFile = env.readCsvFile(inFilePath);
+        DataSource<String> source = env.readTextFile(inFilePath);
+        source.writeAsText(outFilePath);
+        try {
+            env.execute("SlideProgram1");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
